7990264280552942c39fab04e85ecd9f482d5a1c
Travis Brown <travisrobertbrown@gmail.com>
2011-03-03 17:32:26 -0600

    Added some model insertion methods.

--- src/main/scala/edu/umd/mith/cc/model/Text.scala
+++ src/main/scala/edu/umd/mith/cc/model/Text.scala
@@ -7,6 +7,19 @@ import _root_.net.liftweb.common._
 
 object Collection extends Collection with LongKeyedMetaMapper[Collection] {
   override def dbTableName = "collections" // define the DB table name
+
+  override def dbIndexes = Index(IndexField(name)) :: Nil
+
+  def findOrAdd(name: String): Collection = {
+    val existing = this.findAll(By(Collection.name, name))
+    if (existing.isEmpty) {
+      val collection = this.create.name(name)
+      collection.save
+      collection
+    } else {
+      existing.head
+    }
+  }
 }
 
 class Collection extends LongKeyedMapper[Collection] with IdPK {
@@ -25,6 +38,16 @@ object Text extends Text with LongKeyedMetaMapper[Text] {
                            Index(IndexField(author)) ::
                            Index(IndexField(year)) :: Nil
                            //Index(IndexField(collection)) :: Nil
+
+  def add(collection: String, uid: String, title: String, author: String, year: Int): Text = {
+    val text = this.create.collection(Collection.findOrAdd(collection))
+                   .uid(uid)
+                   .title(title)
+                   .author(author)
+                   .year(year)
+    text.save
+    text
+  }
 }
 
 class Text extends LongKeyedMapper[Text] with IdPK {
@@ -46,6 +69,14 @@ object Document extends Document with LongKeyedMetaMapper[Document] {
   override def dbIndexes = Index(IndexField(uid)) ::
                            Index(IndexField(plain)) :: Nil
                            //Index(IndexField(text)) :: Nil
+  def add(text: Text, uid: String, plain: String, html: String) = {
+    val document = this.create.text(text)
+                              .uid(uid)
+                              .plain(plain)
+                              .html(html)
+    document.save
+    document
+  }
 }
 
 class Document extends LongKeyedMapper[Document] with IdPK {
@@ -54,10 +85,29 @@ class Document extends LongKeyedMapper[Document] with IdPK {
   object plain extends MappedText(this)
   object html extends MappedText(this)
   object text extends MappedLongForeignKey(this, Text)
+
+  def setFeatures(values: Array[Double]) {
+    values.zipWithIndex.foreach {
+      case (value, i) => Feature.set(this, i + 1, value)
+    }
+  }
 }
 
 object Feature extends Feature with LongKeyedMetaMapper[Feature] {
   override def dbTableName = "features" // define the DB table name
+
+  def set(document: Document, dim: Int, value: Double): Boolean = {
+    val existing = this.findAll(By(Feature.document, document), By(Feature.dim, dim))
+    if (existing.isEmpty) {
+      val feature = this.create.document(document)
+                               .dim(dim)
+                               .value(value)
+      feature.save
+      false
+    } else {
+      true
+    }
+  }
 }
 
 class Feature extends LongKeyedMapper[Feature] with IdPK {
