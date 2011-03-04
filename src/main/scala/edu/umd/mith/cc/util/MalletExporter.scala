a8a209460062b716089ce2411cb95aa217fbac91
Travis Brown <travisrobertbrown@gmail.com>
2011-03-03 23:11:17 -0600

    Added Colt dependency.

--- src/main/scala/edu/umd/mith/cc/util/MalletExporter.scala
+++ src/main/scala/edu/umd/mith/cc/util/MalletExporter.scala
@@ -1,19 +1,20 @@
 package edu.umd.mith.cc {
 package util {
 
+import java.io._
 import _root_.bootstrap.liftweb.Boot
 import _root_.net.liftweb.mapper._
 
 import edu.umd.mith.cc.model._
 
 class MalletExporter(collection: String, count: Int) {
-  def printSamples() {
+  def printSamples(writer: BufferedWriter) {
     val coll = Collection.findAll(By(Collection.name, collection))(0)
     val texts = Text.findAll(By(Text.collection, coll.id), OrderBy(Text.uuid, Ascending), MaxRows(count))
     texts.foreach { text =>
       val documents = Document.findAll(By(Document.text, text.id), OrderBy(Document.id, Ascending))
       documents.foreach { document =>
-        printf("%s#%s _ %s\n", text.uid.is, document.uid.is, document.plain.is.replaceAll("""\s+""", " "))
+        writer.write("%s#%s _ %s\n".format(text.uid.is, document.uid.is, document.plain.is.replaceAll("""\s+""", " ")))
       }
     }
   }
@@ -24,7 +25,7 @@ object MalletExporter {
     val boot = new bootstrap.liftweb.Boot
     boot.boot
     val exporter = new MalletExporter(args(0), args(1).toInt)
-    exporter.printSamples
+    exporter.printSamples(new BufferedWriter(new FileWriter(args(2))))
   }
 }
 
