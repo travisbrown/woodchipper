323db22dad1282d00be51467d00dbcf754af6cec
Sayan Bhatatcharyya <ok.president@gmail.com>
2011-06-16 14:09:01 -0400

    rendering of piechart now in drilldown part, but piechart not showing up for some reason

--- src/main/resources/toserve/js/viz/pca.js
+++ src/main/resources/toserve/js/viz/pca.js
@@ -105,163 +105,160 @@ jQuery("#pca-viz-variance").bind("plotclick", function (event, pos, item) {
 });
 
 
-// var series = Math.floor(Math.random()*10)+1;
-// for( var i = 0; i<series; i++){
-//		pie_data[i] = { "label": "Series"+(i+1), "data": Math.floor(Math.random()*100)+1 };
-//}
+/***** Pie chart stuff *******/
 
-// For now, choose the number of significant topics per document to be FOUR (hardcoded). This needs to be parametrized later.
-var no_of_significant_topics_per_document=4;
+function pie(data) {
 
-var pie_data = new Array(no_of_significant_topics_per_document+1);
-var pietopic = new Array(no_of_significant_topics_per_document);
-var most_significant_topics_for_this_document  = [];
-var probabilities_of_the_most_significant_topics_for_this_document = [];
+	// For now, choose the number of significant topics per document to be FOUR (hardcoded). This needs to be parametrized later.
+	var no_of_significant_topics_per_document=4;
 
-// pca_viz.topics contains ALL the topics, in a specific order.
-// The distributions of all those topics are in the array 
-// data.document.features, in the same order of topics as the 
-// order of topics in pca_viz.topics
+	var pie_data = new Array(no_of_significant_topics_per_document+1);
+	var pietopic = new Array(no_of_significant_topics_per_document);
+	var most_significant_topics_for_this_document  = [];
+	var probabilities_of_the_most_significant_topics_for_this_document = [];
 
-// We want to pick the topics with the highest distributions. 
-// So we will iterate through all the topics in pca_viz.topics, 
-// picking the top FOUR, and simultaneously keeping track of the indices
-// of those FOUR. (Recall that FOUR is the number of topics, hard-coded 
-// for now, which will be parametrized later.
+	// pca_viz.topics contains ALL the topics, in a specific order.
+	// The distributions of all those topics are in the array 
+	// data.document.features, in the same order of topics as the 
+	// order of topics in pca_viz.topics
 
-// Once those FOUR topics with the highest values have been found
-// from data.document.features, we will read off what those FOUR
-// topics were, from looking up the topics corresponding to those
-// FOUR indices from pca_viz.topics. These FOUR topics will then form 
-// the labels of the pie chart, while the actual distributions corresponding
-// to these FOUR topics will form FOUR of the FIVE segments of the pie
-// chart (the additional segment having the catch-all label of "Other",
-// and having a content of (1- (sum of the probabilities of the other FOUR
-// contents)).
+	// We want to pick the topics with the highest distributions. 
+	// So we will iterate through all the topics in pca_viz.topics, 
+	// picking the top FOUR, and simultaneously keeping track of the indices
+	// of those FOUR. (Recall that FOUR is the number of topics, hard-coded 
+	// for now, which will be parametrized later.
+	
+	// Once those FOUR topics with the highest values have been found
+	// from data.document.features, we will read off what those FOUR
+	// topics were, from looking up the topics corresponding to those
+	// FOUR indices from pca_viz.topics. These FOUR topics will then form 
+	// the labels of the pie chart, while the actual distributions corresponding
+	// to these FOUR topics will form FOUR of the FIVE segments of the pie
+	// chart (the additional segment having the catch-all label of "Other",
+	// and having a content of (1- (sum of the probabilities of the other FOUR
+	// contents)).
+	
+	// Obtain the number of topics from pca_viz.topics -- this number is
+	// equal to the number of elements in the pca_viz.topics array.
+	// Maybe this will need to be parametrized later.
+	
+	var no_of_topics = pca_viz.topics.length;
+	
+	var no_of_words_constituting_each_topic = (pca_viz.topics[0]).length;
+	
+	// For the moment, find the most significant topics for the document 
+	// by a brute-force-search. We will refine this to a more clever 
+	// method later.
+	
+	var index_of_topic_with_currently_highest_probability = 0;
+	var index_of_topic_with_currently_second_highest_probability = 0;
+	var index_of_topic_with_currently_third_highest_probability = 0;
+	var index_of_topic_with_currently_fourth_highest_probability = 0;
+	
+	var probability_of_topic_with_currently_highest_probability=0;
+	var probability_of_topic_with_currently_second_highest_probability=0;
+	var probability_of_topic_with_currently_third_highest_probability=0;
+	var probability_of_topic_with_currently_fourth_highest_probability=0;    
+		   	   
+	// while sorting the entire data.document.features array would have been conceptually simpler,
+	// it would have been less inefficient than necessary; all we really care about here is to find
+	// the top m topics -- we don't need to sort the entire array; all we are doing below is to 
+	// store, sorted, just the top m topics at any given instant, as we iterate through the data.document.features 
+	// array
+	
+	var temp, tempindex;        
+	for (var i = 0; i < no_of_topics ; i++) {
+			if (data.document.features[i] > probability_of_topic_with_currently_fourth_highest_probability) {
+				index_of_topic_with_currently_fourth_highest_probability = i;
+				probability_of_topic_with_currently_fourth_highest_probability = data.document.features[i];
+			}
+			if (probability_of_topic_with_currently_fourth_highest_probability >
+				 probability_of_topic_with_currently_third_highest_probability) {
+				 
+				 // interchange the topics with currently the fourth and third highest probabilities
+				 
+				 temp = probability_of_topic_with_currently_fourth_highest_probability;
+				 probability_of_topic_with_currently_fourth_highest_probability=probability_of_topic_with_currently_third_highest_probability;
+				 probability_of_topic_with_currently_third_highest_probability=temp;
+				 
+				 tempindex = index_of_topic_with_currently_fourth_highest_probability;
+				 index_of_topic_with_currently_fourth_highest_probability=index_of_topic_with_currently_third_highest_probability;
+				 index_of_topic_with_currently_third_highest_probability=tempindex;
+				 
+			} 
+			if (probability_of_topic_with_currently_third_highest_probability >
+				 probability_of_topic_with_currently_second_highest_probability) {
+				 
+				 // interchange the topics with currently the third and second highest probabilities
+				 
+				 temp = probability_of_topic_with_currently_third_highest_probability;
+				 probability_of_topic_with_currently_third_highest_probability=probability_of_topic_with_currently_second_highest_probability;
+				 probability_of_topic_with_currently_second_highest_probability=temp;
+				 
+				 tempindex = index_of_topic_with_currently_third_highest_probability;
+				 index_of_topic_with_currently_third_highest_probability=index_of_topic_with_currently_second_highest_probability;
+				 index_of_topic_with_currently_second_highest_probability=tempindex;
+			} 
+			if (probability_of_topic_with_currently_second_highest_probability >
+				 probability_of_topic_with_currently_highest_probability) {
+				 
+				 // interchange the topics with currently the second highest and highest probabilities
+				 
+				 temp = probability_of_topic_with_currently_second_highest_probability;
+				 probability_of_topic_with_currently_second_highest_probability=probability_of_topic_with_currently_highest_probability;
+				 probability_of_topic_with_currently_highest_probability=temp;
+				 
+				 tempindex = index_of_topic_with_currently_second_highest_probability;
+				 index_of_topic_with_currently_second_highest_probability=index_of_topic_with_currently_highest_probability;
+				 index_of_topic_with_currently_highest_probability=tempindex;
+			} 
+	}        
+	
+	most_significant_topics_for_this_document.push(data.document.features[index_of_topic_with_currently_highest_probability]);
+	most_significant_topics_for_this_document.push(data.document.features[index_of_topic_with_currently_second_highest_probability]);
+	most_significant_topics_for_this_document.push(data.document.features[index_of_topic_with_currently_third_highest_probability]);
+	most_significant_topics_for_this_document.push(data.document.features[index_of_topic_with_currently_fourth_highest_probability]);
+	
+	probabilities_of_the_most_significant_topics_for_this_document.push(probability_of_topic_with_currently_highest_probability);
+	probabilities_of_the_most_significant_topics_for_this_document.push(probability_of_topic_with_currently_second_highest_probability);
+	probabilities_of_the_most_significant_topics_for_this_document.push(probability_of_topic_with_currently_third_highest_probability);
+	probabilities_of_the_most_significant_topics_for_this_document.push(probability_of_topic_with_currently_fourth_highest_probability);
+	
+	for( var i = 0; i<no_of_significant_topics_per_document; i++){
+			
+			topic[i] = [];
+			for (var j = 0; j < no_of_words_constituting_each_topic; j++) {
+				  pietopic[i].push(most_significant_topics_for_this_document[j].slice(0,5).join(" ") );
+			 }		  
+			 pie_data[i] = { "label": pietopic[i], "data": probabilities_of_the_most_significant_topics_for_this_document[i] };
+	}
+	
+	var sum_of_the_most_significant_probabilities_for_this_document = 0;
+	for( var i = 0; i<no_of_significant_topics_per_document; i++){
+			sum_of_the_most_significant_probabilities_for_this_document += probabilities_of_the_most_significant_topics_for_this_document[i];
+	}		
+	pie_data[i+1] = { "label": "Other", "data": (1-sum_of_the_most_significant_probabilities_for_this_document) };
+	
+	alert(pie_data);
+	
+	var pca_viz_piechart = {
+	  "series": { 
+		  "pie": {
+			"show": true
+		  }
+	  },  
+	};
+	
+	
+	jQuery("#pca-viz-piechart").addClass("flot_lww");
+	jQuery("#pca-viz-piechart").show();
+	
+	
+	jQuery.plot(jQuery("#pca-viz-piechart"), pie_data, pca_viz_piechart);
+	
+	// jQuery(document).ready(function() { pca_viz_plot_piechart(pie_data);; });
 
-/* for( var i = 0; i<no_of_topics; i++){
-        for (var j = 0; j < 5; j++) {
-   		      pietopic.push(pca_viz.topics[j].slice(0,5).join(" ") );
-         }		  
-         //pietopic.push({
-             // eventually, this should be replaced by the actual features (sorted)
-             // for now, it's randomly generated (for testing).
-    	     // "data": Math.floor(Math.random()*100)+1
-         //});
-		 pie_data[i] = { "label": pietopic[i], "data": Math.floor(Math.random()*100)+1 };
-} */
-
-
-// Obtain the number of topics from pca_viz.topics -- this number is
-// equal to the number of elements in the pca_viz.topics array.
-// Maybe this will need to be parametrized later.
-
-var no_of_topics = pca_viz.topics.length;
-
-var no_of_words_constituting_each_topic = (pca_viz.topics[0]).length;
-
-// For the moment, find the most significant topics for the document 
-// by a brute-force-search. We will refine this to a more clever 
-// method later.
-
-var index_of_topic_with_currently_highest_probability = 0;
-var index_of_topic_with_currently_second_highest_probability = 0;
-var index_of_topic_with_currently_third_highest_probability = 0;
-var index_of_topic_with_currently_fourth_highest_probability = 0;
-
-var probability_of_topic_with_currently_highest_probability=0;
-var probability_of_topic_with_currently_second_highest_probability=0;
-var probability_of_topic_with_currently_third_highest_probability=0;
-var probability_of_topic_with_currently_fourth_highest_probability=0;    
-       
-       
-       
-// while sorting the entire data.document.features array would have been conceptually simpler,
-// it would have been less inefficient than necessary; all we really care about here is to find
-// the top m topics -- we don't need to sort the entire array; all we are doing below is to 
-// store, sorted, just the top m topics at any given instant, as we iterate through the data.document.features 
-// array
-
-var temp;        
-for (var i = 0; i < no_of_topics ; i++) {
-        if (data.document.features[i] > probability_of_topic_with_currently_fourth_highest_probability) {
-        	index_of_topic_with_currently_fourth_highest_probability = i;
-        	probability_of_topic_with_currently_fourth_highest_probability = data.document.features[i];
-        }
-        if (probability_of_topic_with_currently_fourth_highest_probability >
-             probability_of_topic_with_currently_third_highest_probability) {
-             
-             // interchange the topics with currently the fourth and third highest probabilities
-             
-             temp = probability_of_topic_with_currently_fourth_highest_probability;
-             probability_of_topic_with_currently_fourth_highest_probability=probability_of_topic_with_currently_third_highest_probability;
-             probability_of_topic_with_currently_third_highest_probability=temp;
-        } 
-        if (probability_of_topic_with_currently_third_highest_probability >
-             probability_of_topic_with_currently_second_highest_probability) {
-             
-             // interchange the topics with currently the third and second highest probabilities
-             
-             temp = probability_of_topic_with_currently_third_highest_probability;
-             probability_of_topic_with_currently_third_highest_probability=probability_of_topic_with_currently_second_highest_probability;
-             probability_of_topic_with_currently_second_highest_probability=temp;
-        } 
-        if (probability_of_topic_with_currently_second_highest_probability >
-             probability_of_topic_with_currently_highest_probability) {
-             
-             // interchange the topics with currently the second highest and highest probabilities
-             
-             temp = probability_of_topic_with_currently_second_highest_probability;
-             probability_of_topic_with_currently_second_highest_probability=probability_of_topic_with_currently_highest_probability;
-             probability_of_topic_with_currently_highest_probability=temp;
-        } 
-}        
-
-most_significant_topics_for_this_document.push(data.document.features[index_of_topic_with_currently_highest_probability]);
-most_significant_topics_for_this_document.push(data.document.features[index_of_topic_with_currently_second_highest_probability]);
-most_significant_topics_for_this_document.push(data.document.features[index_of_topic_with_currently_third_highest_probability]);
-most_significant_topics_for_this_document.push(data.document.features[index_of_topic_with_currently_fourth_highest_probability]);
-
-probabilities_of_the_most_significant_topics_for_this_document.push(probability_of_topic_with_currently_highest_probability);
-probabilities_of_the_most_significant_topics_for_this_document.push(probability_of_topic_with_currently_second_highest_probability);
-probabilities_of_the_most_significant_topics_for_this_document.push(probability_of_topic_with_currently_third_highest_probability);
-probabilities_of_the_most_significant_topics_for_this_document.push(probability_of_topic_with_currently_fourth_highest_probability);
-
-for( var i = 0; i<no_of_significant_topics_per_document; i++){
-        pietopic[i] = [];
-        for (var j = 0; j < no_of_words_constituting_each_topic; j++) {
-   		      pietopic[i].push(most_significant_topics_for_this_document[j].slice(0,5).join(" ") );
-         }		  
-		 pie_data[i] = { "label": pietopic[i], "data": probabilities_of_the_most_significant_topics_for_this_document[i] };
-}
-
-var sum_of_the_most_significant_probabilities_for_this_document = 0;
-for( var i = 0; i<no_of_significant_topics_per_document; i++){
-		sum_of_the_most_significant_probabilities_for_this_document += probabilities_of_the_most_significant_topics_for_this_document[i];
-}		
-pie_data[i+1] = { "label": "Other", "data": (1-sum_of_the_most_significant_probabilities_for_this_document) };
-
-
-var pca_viz_piechart = {
- //"label": "",
-  "series": { 
-  	  "pie": {
-        "show": true
-  	  }
-  },  
- //"data": data
-};
-
-function pca_viz_plot_piechart() {
-    
-    jQuery.plot(jQuery("#pca-viz-piechart"), pie_data, pca_viz_piechart);
-}
-
-
-jQuery("#pca-viz-piechart").addClass("flot_lww");
-jQuery("#pca-viz-piechart").show();
-
-jQuery(document).ready(function() { pca_viz_plot_piechart();; });
+} // end of function "pie"
 
 
 
@@ -320,6 +317,9 @@ var pca_load_document = function(event, pos, item) {
       jQuery('#drilldown-link').text("Visit source collection");
       jQuery('#drilldown-link').attr("href", data.document.url);
       jQuery('#drilldown-text').html(data.document.html);
+      jQuery('#pca-viz-piechart').addClass("flot_lww");
+      jQuery('#pca-viz-piechart').show();
+      pie(data);
     });
   }
 };
