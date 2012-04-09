f3706b569d047d26649bb08144f510dab6937d87
Travis Brown <travisrobertbrown@gmail.com>
2012-04-09 13:51:51 -0400

    A little more work on cleaning up the boot script.

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -1,43 +1,28 @@
 package bootstrap.liftweb
 
-import _root_.net.liftweb.util._
-import _root_.net.liftweb.common._
-import _root_.net.liftweb.http._
-import _root_.net.liftweb.http.ResourceServer
-import _root_.net.liftweb.http.provider._
-import _root_.net.liftweb.json.JsonAST._
-import _root_.net.liftweb.json.JsonDSL._
-import _root_.net.liftweb.mapper._
-import _root_.net.liftweb.sitemap._
-import _root_.net.liftweb.sitemap.Loc._
-import Helpers._
-import _root_.net.liftweb.mapper.{DB, ConnectionManager, Schemifier, DefaultConnectionIdentifier, StandardDBVendor}
-import _root_.java.sql.{Connection, DriverManager}
+import net.liftweb.util.Props
+import net.liftweb.common.Full
+import net.liftweb.http.{ LiftRules, ResourceServer, S }
+import net.liftweb.http.provider.HTTPRequest
+import net.liftweb.sitemap.{ Menu, SiteMap }
+import net.liftweb.sitemap.Loc.Hidden
+import net.liftweb.mapper.{ DB, ConnectionManager, Schemifier, DefaultConnectionIdentifier, StandardDBVendor }
+import java.sql.{ Connection, DriverManager }
 import edu.umd.mith.cc.model._
-//import net.liftweb.widgets.flot._
 
-/**
- * A class that's instantiated early and run.  It allows the application
- * to modify lift's environment
- */
 class Boot {
   def boot {
     if (!DB.jndiJdbcConnAvailable_?) {
-      val vendor = 
-	new StandardDBVendor(Props.get("db.driver") openOr "org.h2.Driver",
-			     Props.get("db.url") openOr 
-			     "jdbc:h2:lift_proto.db;AUTO_SERVER=TRUE",
-			     Props.get("db.user"), Props.get("db.password"))
+      val vendor = new StandardDBVendor(
+        Props.get("db.driver") openOr "org.h2.Driver",
+			  Props.get("db.url") openOr 
+			  "jdbc:h2:lift_proto.db;AUTO_SERVER=TRUE",
+			  Props.get("db.user"), Props.get("db.password")
+      )
 
       LiftRules.unloadHooks.append(vendor.closeAllConnections_! _)
-
       DB.defineConnectionManager(DefaultConnectionIdentifier, vendor)
     }
-
-
-    //Flot.init
-    //LiftRules.htmlProperties.default.set((r: Req) =>
-    //  new Html5Properties(r.userAgent))
 
     ResourceServer.allow({
       case "js" :: "viz" :: "pca.js" :: Nil => true
@@ -47,37 +32,24 @@ class Boot {
       case "css" :: "default.css" :: Nil => true
     })
 
-    // where to search snippet
     LiftRules.addToPackages("edu.umd.mith.cc")
     Schemifier.schemify(true, Schemifier.infoF _, User, Collection, Text, Document, Feature, Topic, Word, TopicWord)
 
-    // Build SiteMap
-    def sitemap() = SiteMap(
-      Menu.i("Home") / "index" >> User.AddUserMenusAfter, // Simple menu form
+    val sitemap = SiteMap(
+      Menu.i("Home") / "index" >> User.AddUserMenusAfter,
       Menu.i("Search") / "search",
       Menu.i("Viz") / "viz" >> Hidden,
       Menu.i("Remove") / "remove" >> Hidden,
       Menu.i("Add") / "add" >> Hidden,
       Menu.i("Drilldown") / "drilldown" >> Hidden
-      // Menu with special Link
-      /*Menu(/*Loc("Static", Link(List("static"), true, "/static/index"), 
-	       "Static Content")*/)*/)
+    )
 
-    LiftRules.setSiteMapFunc(() => User.sitemapMutator(sitemap()))
+    LiftRules.setSiteMapFunc(() => User.sitemapMutator(sitemap))
 
-    /*
-     * Show the spinny image when an Ajax call starts
-     */
-    LiftRules.ajaxStart =
-      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
+    LiftRules.ajaxStart = Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
+    LiftRules.ajaxEnd = Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
 
-    /*
-     * Make the spinny image go away when it ends
-     */
-    LiftRules.ajaxEnd =
-      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
-
-    LiftRules.early.append(makeUtf8)
+    LiftRules.early.append(this.makeUtf8)
 
     LiftRules.loggedInTest = Full(() => User.loggedIn_?)
     LiftRules.statelessDispatchTable.append(edu.umd.mith.cc.services.RestService)
@@ -92,3 +64,4 @@ class Boot {
     req.setCharacterEncoding("UTF-8")
   }
 }
+
